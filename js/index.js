08951d6bb1fedff398992c318ec0c4836c8979ee
diana9015 <yunjjj90@gmail.com>
2022-04-06 01:32:05 +0900

    0406_1

--- js/index.js
+++ js/index.js
@@ -124,3 +124,17 @@ logClose.addEventListener("click", (e) => {
 
   logPop.style.opacity = 0;
 });
+
+const btnCall = document.querySelector(".btnCall");
+const menuMo = document.querySelector(".menuMo");
+
+//btnCall을 클릭했을 때
+btnCall.onclick = function (e) {
+  //링크이동금지
+  e.preventDefault();
+
+  //btnCall에 on이 있으면 제거, 없으면 추가
+  btnCall.classList.toggle("on");
+  //menuMo에 on이 있으면 제거, 없으면 추가
+  menuMo.classList.toggle("on");
+};
