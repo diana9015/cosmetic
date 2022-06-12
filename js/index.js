ed94c7106532b45f947c10e7b2cfa8cf2d3cf88e
diana9015 <yunjjj90@gmail.com>
2022-06-12 17:32:28 +0900

    0612

--- js/index.js
+++ js/index.js
@@ -1,140 +1,147 @@
-const header = document.querySelector("header");
-const popup = header.querySelector(".popup");
-const btnClose = popup.querySelector(".btnclose");
-const isCookie = document.cookie.indexOf("today=done");
+const header = document.querySelector('header');
+const popup = header.querySelector('.popup');
+const btnClose = popup.querySelector('.btnclose');
+const isCookie = document.cookie.indexOf('today=done');
 let isOn;
 
-isCookie == -1 ? (isOn = "block") : (isOn = "none");
+isCookie == -1 ? (isOn = 'block') : (isOn = 'none');
 popup.style.display = isOn;
 
-btnClose.addEventListener("click", (e) => {
-  e.preventDefault();
-  //체크박스에 체크가 되어있으면
-  //쿠키를 생성
-  let isChecked = popup.querySelector("input[type=checkbox]").checked;
-  if (isChecked) setCookie("today", "done", 1);
+btnClose.addEventListener('click', (e) => {
+	e.preventDefault();
+	//체크박스에 체크가 되어있으면
+	//쿠키를 생성
+	let isChecked = popup.querySelector('input[type=checkbox]').checked;
+	if (isChecked) setCookie('today', 'done', 1);
 
-  popup.style.display = "none";
+	popup.style.display = 'none';
 });
 
 // 쿠키 생성 함수(쿠키를 만들어놓음 =)
 function setCookie(name, val, due) {
-  const today = new Date();
-  const day = today.getDate();
-  today.setDate(day + due);
-  const duedate = today.toGMTString();
-  document.cookie = `${name}=${val}; path=/; expires=${duedate}`;
+	const today = new Date();
+	const day = today.getDate();
+	today.setDate(day + due);
+	const duedate = today.toGMTString();
+	document.cookie = `${name}=${val}; path=/; expires=${duedate}`;
 }
 
-const slider = document.querySelector("#slider");
-const ul = slider.querySelector("ul");
-const prev = slider.querySelector(".prev");
-const next = slider.querySelector(".next");
+const slider = document.querySelector('#slider');
+const ul = slider.querySelector('ul');
+const prev = slider.querySelector('.prev');
+const next = slider.querySelector('.next');
 const speed = 1000;
 let enableClick = true;
 
 //페이지가 로딩되었을때 ul의 초기값 -100%로 설정
-ul.style.left = "-100%";
+ul.style.left = '-100%';
 //왜 셀렉트올이 아닌가..?
 
 //next버튼을 클릭했을때
-next.addEventListener("click", (e) => {
-  e.preventDefault();
+next.addEventListener('click', (e) => {
+	e.preventDefault();
 
-  if (enableClick) {
-    enableClick = false;
-    nextSlide();
-  }
-  //   //ul의 left값을 변경해야함. -100%
+	if (enableClick) {
+		enableClick = false;
+		nextSlide();
+	}
+	//   //ul의 left값을 변경해야함. -100%
 
-  //   //new Anim(우리가 움직여야할 요소,anim)
+	//   //new Anim(우리가 움직여야할 요소,anim)
 
-  //   new Anim(ul, {
-  //     prop: "left",
-  //     value: "-200%",
-  //     duration: 1000,
-  //     //callback은 다시 하는거
-  //     callback: () => {
-  //       ul.style.left = "-100%";
-  //       ul.append(ul.firstElementChild);
-  //     },
-  //   });
+	//   new Anim(ul, {
+	//     prop: "left",
+	//     value: "-200%",
+	//     duration: 1000,
+	//     //callback은 다시 하는거
+	//     callback: () => {
+	//       ul.style.left = "-100%";
+	//       ul.append(ul.firstElementChild);
+	//     },
+	//   });
 });
 
-prev.addEventListener("click", (e) => {
-  e.preventDefault();
+prev.addEventListener('click', (e) => {
+	e.preventDefault();
 
-  if (enableClick) {
-    prevSlide();
-    enableClick = false;
-  }
-  //   new Anim(ul, {
-  //     prop: "left",
-  //     value: "0%",
-  //     duration: 1000,
+	if (enableClick) {
+		prevSlide();
+		enableClick = false;
+	}
+	//   new Anim(ul, {
+	//     prop: "left",
+	//     value: "0%",
+	//     duration: 1000,
 
-  //     callback: () => {
-  //       ul.style.left = "-100%";
-  //       ul.prepend(ul.lastElementChild);
-  //     },
-  //   });
+	//     callback: () => {
+	//       ul.style.left = "-100%";
+	//       ul.prepend(ul.lastElementChild);
+	//     },
+	//   });
 });
 
 //---------------------------함수로 변환--
 function nextSlide() {
-  new Anime(ul, {
-    prop: "left",
-    value: "-200%",
-    duration: 1000,
+	new Anime(ul, {
+		prop: 'left',
+		value: '-200%',
+		duration: 1000,
 
-    callback: () => {
-      ul.style.left = "-100%";
-      ul.append(ul.firstElementChild);
-      enableClick = true;
-    },
-  });
+		callback: () => {
+			ul.style.left = '-100%';
+			ul.append(ul.firstElementChild);
+			enableClick = true;
+		},
+	});
 }
 
 function prevSlide() {
-  new Anime(ul, {
-    prop: "left",
-    value: "0%",
-    duration: 1000,
+	new Anime(ul, {
+		prop: 'left',
+		value: '0%',
+		duration: 1000,
 
-    callback: () => {
-      ul.style.left = "-100%";
-      ul.prepend(ul.lastElementChild);
-      enableClick = true;
-    },
-  });
+		callback: () => {
+			ul.style.left = '-100%';
+			ul.prepend(ul.lastElementChild);
+			enableClick = true;
+		},
+	});
 }
 
-const logBtn = header.querySelector(".logopen");
-const logPop = header.querySelector(".loginpop");
-const logClose = header.querySelector(".logclose");
+const logBtn = header.querySelector('.logopen');
+const logPop = header.querySelector('.loginpop');
+const logClose = header.querySelector('.logclose');
 
-logBtn.addEventListener("click", (e) => {
-  e.preventDefault();
-
-  logPop.style.opacity = 0.8;
+logBtn.addEventListener('click', (e) => {
+	e.preventDefault();
+	logPop.style.display = 'block';
 });
 
-logClose.addEventListener("click", (e) => {
-  e.preventDefault();
+logClose.addEventListener('click', (e) => {
+	e.preventDefault();
 
-  logPop.style.opacity = 0;
+	logPop.style.display = 'none';
 });
 
-const btnCall = document.querySelector(".btnCall");
-const menuMo = document.querySelector(".menuMo");
+const btnCall = document.querySelector('.btnCall');
+const menuMo = document.querySelector('.mob_nav');
 
 //btnCall을 클릭했을 때
 btnCall.onclick = function (e) {
-  //링크이동금지
-  e.preventDefault();
+	//링크이동금지
+	e.preventDefault();
 
-  //btnCall에 on이 있으면 제거, 없으면 추가
-  btnCall.classList.toggle("on");
-  //menuMo에 on이 있으면 제거, 없으면 추가
-  menuMo.classList.toggle("on");
+	//btnCall에 on이 있으면 제거, 없으면 추가
+	btnCall.classList.toggle('on');
+	//menuMo에 on이 있으면 제거, 없으면 추가
+	menuMo.classList.toggle('on');
 };
+
+btnCall.addEventListener('click', () => {
+	if (menuMo.style.right == '8px') {
+		menuMo.style.right = '-100%';
+	} else {
+		menuMo.style.right = '8px';
+	}
+});
--- js/infor.js
+++ js/infor.js
@@ -2,33 +2,33 @@
 
 //https://www.youtube.com/playlist?list=PLSGs9D6sndZf2JAmaS-YSDJj_e9idSilg
 
-const body = document.querySelector("body");
-const main = document.querySelector("main");
-const key = "AIzaSyAfcjz6O74NPHtRR-KvH7LyGwudwwoNIN8";
-const playListId = "PLYOPkdUKSFgWPLsAWpqRpK0cCiAGdxi-Y";
+const body = document.querySelector('body');
+const main = document.querySelector('main');
+const key = 'AIzaSyAfcjz6O74NPHtRR-KvH7LyGwudwwoNIN8';
+const playListId = 'PLYOPkdUKSFgWPLsAWpqRpK0cCiAGdxi-Y';
 const num = 4;
-const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&key=${"AIzaSyAfcjz6O74NPHtRR-KvH7LyGwudwwoNIN8"}&playlistId=${"PLYOPkdUKSFgWPLsAWpqRpK0cCiAGdxi - Y"}&maxResults=${num}`;
+const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&key=${'AIzaSyAfcjz6O74NPHtRR-KvH7LyGwudwwoNIN8'}&playlistId=${'PLYOPkdUKSFgWPLsAWpqRpK0cCiAGdxi - Y'}&maxResults=${num}`;
 
 fetch(url)
-  .then((data) => {
-    return data.json();
-  })
-  .then((json) => {
-    let items = json.items;
-    console.log(items);
+	.then((data) => {
+		return data.json();
+	})
+	.then((json) => {
+		let items = json.items;
+		console.log(items);
 
-    let result = "";
+		let result = '';
 
-    items.forEach((item) => {
-      // let tit = item.snippet.title;
-      // let desc = item.snippet.description;
-      // if(tit.length > 50) tit = tit.substr(0,50)+"...";
-      // if(desc.length > 150) desc = desc.substr(0,150)+"...";
+		items.forEach((item) => {
+			// let tit = item.snippet.title;
+			// let desc = item.snippet.description;
+			// if(tit.length > 50) tit = tit.substr(0,50)+"...";
+			// if(desc.length > 150) desc = desc.substr(0,150)+"...";
 
-      // let date = item.snippet.publishedAt.split("T")[0];
-      // console.log(date);
+			// let date = item.snippet.publishedAt.split("T")[0];
+			// console.log(date);
 
-      result += `
+			result += `
                 <article>
                     <a class="pic" href="#" data-vid="${item.snippet.resourceId.videoId}">
                         <img src="${item.snippet.thumbnails.standard.url}">
@@ -40,7 +40,16 @@ fetch(url)
                     </div>                    
                 </article>
         `;
-    });
+		});
 
-    vidinner.innerHTML = result;
-  });
+		vidinner.innerHTML = result;
+	});
+const btnCall = document.querySelector('.btnCall');
+const menuMo = document.querySelector('.mobbtn');
+btnCall.onclick = function (e) {
+	e.preventDefault();
+	btnCall.classList.toggle('on');
+	menuMo.classList.toggle('on');
+};
+
+console.log(menuMo);
